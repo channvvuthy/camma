83f5e35a95c6a18ab3b361ba44ea21549605f7fd
channvuthy <channvuthyit@gmail.com>
2022-10-11 13:49:53 +0700

    Add icon

--- src/background.js
+++ src/background.js
@@ -59,7 +59,7 @@ async function createWindow() {
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
     if (!process.env.IS_loadingScreen) win.webContents.openDevTools();
   } else {
-    win.webContents.openDevTools();
+    createProtocol("app");
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
   }
